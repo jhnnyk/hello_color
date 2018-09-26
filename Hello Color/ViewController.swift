2abd07d6cc1f621e1b33565ff8eeec0c24884717
John Knotts <jhnnyk@gmail.com>
2018-09-25 20:25:12 -0600

    Initial Commit

---
+++ Hello Color/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello Color
+//
+//  Created by John Knotts on 9/25/18.
+//  Copyright © 2018 John Knotts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
